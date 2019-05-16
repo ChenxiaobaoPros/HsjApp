c780c82aa2297ad87ef6cbec06d70da3533ebcc6
chenxiaobao <931234253@qq.com>
2019-05-16 16:54:48 +0800

    字体库

---
+++ HsjAppExample/HsjAppExample/HsjAppExample/IconfontPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Hsj.AppControls.Iconfonts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HsjAppExample
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class IconfontPage : ContentPage
+	{
+		public IconfontPage ()
+		{
+			InitializeComponent ();
+
+            //foreach (var item in Enum.GetValues(typeof(Unicode)))
+            //{
+            //    var labelKey = new Label { Text = $"{(char)item}" };
+
+            //    var labelValue = new Label { Text = $"{(char)item}" };
+
+            //    stackLayout.Children.Add(
+
+
+            //        );
+            //}
+            var items = Enum.GetValues(typeof(Unicode));
+
+            var stackLyout = new StackLayout();
+            for (int i = 0; i < items.Length; i++)
+            {
+                var item = items.GetValue(i);
+                int str = (int)item;
+                char car = (char)str;
+                string strr = $"{car}";
+                var labelValue = new Label { Text = strr,FontSize=30 ,FontFamily= "iconize-entypoplus.ttf#" };
+
+                var labelKey = new Label { Text = item.ToString(), FontSize = 30 };
+
+                var layout = new StackLayout { Orientation =StackOrientation.Horizontal};
+                layout.Children.Add(labelKey);
+                layout.Children.Add(labelValue);
+
+                stackLyout.Children.Add(layout);
+            }
+
+            scrollView.Content = stackLyout;
+        }
+	}
+}
---
+++ HsjAppExample/HsjAppExample/HsjAppExample/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Hsj.AppControls.Iconfonts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HsjAppExample
+{
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            #region 实验
+            //byte[] by=  charToByte('\ue800');
+
+            //string str = $"\ue800"; //$"{char}"; 
+            //this.Content = new Label { Text = str, FontFamily = "iconize-entypoplus.ttf#" };
+            /*********************************************/
+
+            //string str = $"{(char)Unicode.ep_500px}"; //$"{char}"; 
+            //this.Content = new Label { Text = str, FontFamily = "iconize-entypoplus.ttf#" };
+            #endregion
+
+
+        }
+        public static byte[] charToByte(char c)
+        {
+            byte[] b = new byte[2];
+            b[0] = (byte)((c & 0xFF00) >> 8);
+            b[1] = (byte)(c & 0xFF);
+            return b;
+        }
+        public static char byteToChar(byte[] b)
+        {
+            char c = (char)(((b[0] & 0xFF) << 8) | (b[1] & 0xFF));
+            return c;
+        }
+
+        private void IconGrop_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new IconfontPage());
+        }
+    }
+}
